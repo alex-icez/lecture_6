190bf3b3595cf12b88919126eb6655a8b41973fa
alex-icez <alex-icex@mail.ru>
2015-11-05 21:00:48 +0300

    mess

--- src/lecture_6/Sample4.java
+++ src/lecture_6/Sample4.java
@@ -18,6 +18,42 @@ class LinkedList<T> implements List<T> {
 		}
 	}
 
+	@Override
+	public void add(T value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set(int index, T value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void get(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 	
 }
@@ -26,7 +62,7 @@ class LinkedList<T> implements List<T> {
 public class Sample4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
