1de575eee7822a75052f27bd2e442023fe0f47f6
alex-icez <alex-icex@mail.ru>
2015-11-05 20:41:59 +0300

    mess

--- src/lecture_6/Test.java
+++ src/lecture_6/Test.java
@@ -44,9 +44,7 @@ public class Test {
 	}
 	
 	public static void main(String args[]) {
-		Box b1 = new Box(1,2,3);
-		Box b2 = new Box(1,2,3);
-		System.out.println(b1);
+		String arr[] = {"a", "ab", "bb","ba", "bb", "aa", "ba"};
 	
 		
 		
