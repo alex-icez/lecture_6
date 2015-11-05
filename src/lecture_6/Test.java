372579c31d5d4d413c37a1cbbc06d1451b126469
alex-icez <alex-icex@mail.ru>
2015-11-05 20:40:54 +0300

    mess

--- src/lecture_6/Test.java
+++ src/lecture_6/Test.java
@@ -34,8 +34,13 @@ public class Test {
 	
 	static <T extends Comparable<T>> void sort(T arr[]) {
 		T temp;
-		
-		
+		for(int i = 0; i < arr.length; i++)
+			for(int j = 0; j < arr.length - 1 - i; j++)
+				if (arr[j].compareTo(arr[j + 1]) > 0) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}	
 	}
 	
 	public static void main(String args[]) {
