ac46b319e977bab442c9d28330c5ca53d9321add
alex-icez <alex-icex@mail.ru>
2015-11-05 19:23:37 +0300

    mess

--- src/lecture_6/Sample3.java
+++ src/lecture_6/Sample3.java
@@ -2,8 +2,12 @@ package lecture_6;
 
 public class Sample3 {
 
+	<T> void print(T t) {
+		System.out.println(t);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
