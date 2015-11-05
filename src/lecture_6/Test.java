ce246b44e9769d8f58c9fa58b2564a4e4ccf4dc0
alex-icez <alex-icex@mail.ru>
2015-11-05 18:19:23 +0300

    mess

--- src/lecture_6/Test.java
+++ src/lecture_6/Test.java
@@ -4,11 +4,17 @@ class Box {
 	int w;
 	int h;
 	int d;
+	
+	public Box(int w, int h, int d) {
+		this.w = w;
+		this.h = h;
+		this.d = d;
+	}
 }
 
 public class Test {
 	public static void main(String args[]) {
-		
+		Box b 
 		
 	}
 }
