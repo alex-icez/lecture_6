ab20ab39dc50e96dddcd290828ec8434d8b02875
alex-icez <alex-icex@mail.ru>
2015-11-05 19:28:35 +0300

    mess

--- src/lecture_6/Sample3.java
+++ src/lecture_6/Sample3.java
@@ -2,15 +2,15 @@ package lecture_6;
 
 public class Sample3 {
 
-	static Object print(Object t) {
+	static <T> T print(T t) {
 		System.out.println(t);
 		return t;
 	}
 	
 	public static void main(String[] args) {
-		String s = (String)print("fdesfrde");
-		Double d = (Double)print(2134213.0);
-		Box b = (Box)print(new Box(1,2,3));
+		String s = print("fdesfrde");
+		Double d = print(2134213.0);
+		Box i = print(new Box(1,2,3));
 	}
 
 }
