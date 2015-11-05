efe7488fcb7bd512c23c0d5310766d29dc677c54
alex-icez <alex-icex@mail.ru>
2015-11-05 18:21:00 +0300

    mess

--- src/lecture_6/Test.java
+++ src/lecture_6/Test.java
@@ -16,8 +16,9 @@ public class Test {
 	public static void main(String args[]) {
 		Box b1 = new Box(1,2,3);
 		Box b2 = new Box(1,2,3);
-		
-		System.out.println(b1 == b2);
+		String str1 = new String("123");
+		String str2 = new String("123");
+		System.out.println(str1 == str2);
 		
 		
 	}
