62336b4d6092ff1924a032054ed7a3a2925da8b1
alex-icez <alex-icex@mail.ru>
2015-11-05 18:47:01 +0300

    mess

--- src/lecture_6/Sample1.java
+++ src/lecture_6/Sample1.java
@@ -20,7 +20,13 @@ public class Sample1 {
 		
 		for(int i = 0; i < arr.length; i++)
 			System.out.print(arr[i] + " ");
-		System.out.println();	
+		System.out.println();
+		
+		
+		
+		
+		for(int a : arr)
+			System.out.println(a);
 	}
 
 }
