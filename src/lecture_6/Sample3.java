5c59613fbaa6da7e4ed40f00a8415d816482107e
alex-icez <alex-icex@mail.ru>
2015-11-05 20:29:40 +0300

    mess

--- src/lecture_6/Sample3.java
+++ src/lecture_6/Sample3.java
@@ -9,6 +9,7 @@ public class Sample3 {
 
 	static <T extends Number & Comparable<T>> T print(T t) {
 		System.out.println(t.doubleValue());
+		
 		return t;
 	}
 	
@@ -16,6 +17,8 @@ public class Sample3 {
 		print(3213);
 		print(453.0);
 		print("edfs");
+		
+	
 		
 		//	Double d = print(2134213.0);
 	//	Box i = print(new Box(1,2,3));
