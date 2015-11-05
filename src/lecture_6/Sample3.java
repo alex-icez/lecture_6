65684231b956b5dd568194b0b9dff706836b1260
alex-icez <alex-icex@mail.ru>
2015-11-05 20:02:19 +0300

    mess

--- src/lecture_6/Sample3.java
+++ src/lecture_6/Sample3.java
@@ -2,19 +2,20 @@ package lecture_6;
 
 public class Sample3 {
 
-	static <T> T print(T t) {
+	static <T extends Number> T print(T t) {
 		System.out.println(t);
 		return t;
 	}
 	
 	public static void main(String[] args) {
-		Integer s = print();
-
+		print(3213);
+		print(453.0);
+		print("edfs");
 		
 		//	Double d = print(2134213.0);
 	//	Box i = print(new Box(1,2,3));
 		
-		System.out.println(s);
+		//System.out.println(s);
 	}
 
 }
