635c6d47de422b2fb632e3a8b6e21fc5ef077509
alex-icez <alex-icex@mail.ru>
2015-11-05 18:33:37 +0300

    mess

--- src/lecture_6/Sample1.java
+++ src/lecture_6/Sample1.java
@@ -1,10 +1,14 @@
 package lecture_6;
 
+import java.util.Scanner;
+
 public class Sample1 {
 
 	public static void main(String[] args) {
 		int arr[] = new int[4];
 		int count = 0;
+		Scanner s = new Scanner(System.in);
+		
 		
 		
 	}
