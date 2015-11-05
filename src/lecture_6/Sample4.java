74e21d3a0f0a9291838c0ac19e9a671af6a70c89
alex-icez <alex-icex@mail.ru>
2015-11-05 20:57:03 +0300

    mess

--- src/lecture_6/Sample4.java
+++ src/lecture_6/Sample4.java
@@ -1,5 +1,14 @@
 package lecture_6;
 
+interface List<T> {
+	void add(T value);
+	void set(int index, T value);
+	void get(int index);
+	int size();
+	boolean isEmpty();
+	void print();
+}
+
 public class Sample4 {
 
 	public static void main(String[] args) {
